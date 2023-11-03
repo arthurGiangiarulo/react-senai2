e0f6697da1f5633f03d7b559ad9d8325cee74265
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-11-03 12:42:03 -0300

    filtros

--- projeto/src/components/Lista/index.jsx
+++ projeto/src/components/Lista/index.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import ItemDaLista from '../ItemDaLista';
+import CaixaDeSelecao from '../CaixaDeSelecao';
 import './style.css';
 
 export default function Lista(props) {
+    const [unidade, setUnidade] = useState('');
     const [alunos, setAlunos] = useState([
-        { "nome": "Arthur", "url": "https://github.com/arthurGiangiarulo.png", "unidade": "Unidade Centro" }
+        { "nome": "Arthur", "url": "https://github.com/arthurGiangiarulo.png", "unidade": "Unidade Quitandinha" }
     ]);
+    let unidades = [
+        'Unidade Centro',
+        'Unidade Quitandinha',
+        'Unidade Bingen',
+        'Unidade Itamarati',
+        'Unidade Itaipava'
+    ];
 
     return (
-        <ol className='lista'>
-            {alunos.map((aluno, index) => {
-                return (
-                    <div>
-                        <ItemDaLista aluno={aluno} index={index} />
-                    </div>
-                )
-            })}
-        </ol>
+        <div className='lista'>
+            <CaixaDeSelecao 
+                        label='Unidade' 
+                        options={unidades} 
+                        onChange={value => setUnidade(value)}
+            />
+            <ol>
+                {alunos.map((aluno, index) => {
+                    return (
+                        (aluno.unidade === 'Unidade Quitandinha') ?
+                        <div>
+                            <ItemDaLista aluno={aluno} index={index} />
+                        </div>
+                        : null
+                    )
+                })}
+            </ol>
+        </div>
     )
 }
